dd4186d60a6ece9a9ae2053fa69e246c6a5ce6bb
g.berezin <g.berezin@tinkoff.ru>
2024-09-19 18:54:31 +0000

    fix issues

--- src/main/scala/building/Building.scala
+++ src/main/scala/building/Building.scala
@@ -8,36 +8,33 @@ package building
   *     Array), но не меньше 1. Здание всегда должно заканчиваться чердаком На чердаке никто не живет, но это может быть
   *     и коммерческое помещение (но только 1).
   */
-trait Commercial // rewrite me
-
-trait Building // rewrite me
+class Building // rewrite me
 
 trait Floor // rewrite me
-
-trait Attic // rewrite me
-
-trait ResidentialFloor // rewrite me
 
 object Building {
 
   /** Проходится по зданию снизу в вверх, применяя функцию [[f]] на каждом жилом этаже с начальным аккумулятором
     * [[accumulator]]
     */
-  def fold(building: Building, accumulator: Int)(f: (Int, ResidentialFloor) => Int): Int = ???
+  def fold(building: Building, accumulator: Int)(f: (Int, Floor) => Int): Int = ???
 
   /** Подсчитывает количество этаже, на которых живет хотя бы один мужчина старше [[olderThan]]. Используйте [[fold]]
     */
   def countOldManFloors(building: Building, olderThan: Int): Int = ???
 
-  /** Находит наибольший возраст женьщины, проживающей в здании. Используйте [[fold]] */
-  def womanMaxAge(building: Building): Int = ???
+  /** Находит наибольший возраст женщины, проживающей в здании. Используйте [[fold]] */
+  def womanMaxAge(building: Building): Option[Int] = ???
 
   /** Находит кол-во коммерческих заведений в здании. Используйте [[fold]] */
   def countCommercial(building: Building): Int = ???
 
-  /* Находит среднее кол-во коммерческих заведений зданиях. Реализуйте свою функцию, похожую на [[fold]] для прохода по зданию */
+  /** Находит среднее кол-во коммерческих заведений зданиях. Реализуйте свою функцию, похожую на [[fold]] для прохода по
+    * зданию
+    */
   def countCommercialAvg(building: Array[Building]): Double = ???
 
-  /* Находит среднее кол-во мужчин на четных этажах. Реализуйте свою функцию, похожую на [[fold]] для прохода по зданию */
+  /** Находит среднее кол-во мужчин на четных этажах. Реализуйте свою функцию, похожую на [[fold]] для прохода по зданию
+    */
   def evenFloorsMenAvg(building: Building): Double = ???
 }
--- src/test/scala/building/BuildingSpec.scala
+++ src/test/scala/building/BuildingSpec.scala
@@ -4,6 +4,19 @@ import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
 class BuildingSpec extends AnyFlatSpec with Matchers {
+
+  // specs on constructor validations
+
+  "Resident" should "be invalid if age is non positive" in {
+    1 shouldEqual 0 // Implement yourself
+  }
+
+  it should "be invalid if sex is non binary" in {
+    1 shouldEqual 0 // Implement yourself
+  }
+
+  // specs on methods
+
   "countOldManFloors" should "return the number of men older than the specified age" in {
     1 shouldEqual 0 // Implement yourself
   }
@@ -20,7 +33,7 @@ class BuildingSpec extends AnyFlatSpec with Matchers {
     1 shouldEqual 0 // Implement yourself
   }
 
-  it should "return 0 if there are no women in the building" in {
+  it should "return None if there are no women in the building" in {
     1 shouldEqual 0 // Implement yourself
   }
 
@@ -29,6 +42,26 @@ class BuildingSpec extends AnyFlatSpec with Matchers {
   }
 
   it should "return 0 if there are no commercial establishments in the building" in {
+    1 shouldEqual 0 // Implement yourself
+  }
+
+  "countCommercialAvg" should "return average commercial establishments through array of buildings" in {
+    1 shouldEqual 0 // Implement yourself
+  }
+
+  it should "return 0 if there are no commercial establishments in the buildings" in {
+    1 shouldEqual 0 // Implement yourself
+  }
+
+  it should "return 0 if there are no buildings" in {
+    1 shouldEqual 0 // Implement yourself
+  }
+
+  "evenFloorsMenAvg" should "return average count of mens on even floors in the building" in {
+    1 shouldEqual 0 // Implement yourself
+  }
+
+  it should "return 0 if there are no mens in the building" in {
     1 shouldEqual 0 // Implement yourself
   }
 }
