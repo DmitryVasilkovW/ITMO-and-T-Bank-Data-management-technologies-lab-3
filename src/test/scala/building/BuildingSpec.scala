9582d44178fee62e83c81510d7a0dd050a34cbfc
Dmitry Vasilkov <113594620+DmitryVasilkovW@users.noreply.github.com>
2024-09-21 14:33:46 +0300

    test: tests for hw 3

--- src/test/scala/building/BuildingSpec.scala
+++ src/test/scala/building/BuildingSpec.scala
@@ -1,67 +1,79 @@
 package building
 
+import building.Building._
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
 class BuildingSpec extends AnyFlatSpec with Matchers {
 
-  // specs on constructor validations
+  val maleResident: Resident = Resident(40, Male)
+  val femaleResident: Resident = Resident(35, Female)
+  val floorA: ResidentialFloor = ResidentialFloor(maleResident, femaleResident, null)
+  val floorB: ResidentialFloor = ResidentialFloor(maleResident, Resident(30, Female), null)
+  val commercialFloorA: CommercialFloor = CommercialFloor(List(Commercial("Restaurant")), null)
+  val atticA: Attic = CommonAttic
+  val buildingWithMultipleResidents: Building = Building("789 Park Ave", floorA)
+  val buildingWithShops: Building = Building("101 Pine St", commercialFloorA)
+  val simpleAtticBuilding: Building = Building("202 Cedar St", atticA)
 
-  "Resident" should "be invalid if age is non positive" in {
-    1 shouldEqual 0 // Implement yourself
+  "Resident" should "throw exception if age is non-positive" in {
+    assertThrows[IllegalArgumentException] {
+      Resident(0, Male)
+    }
   }
 
-  it should "be invalid if sex is non binary" in {
-    1 shouldEqual 0 // Implement yourself
+  it should "throw exception if gender is non-binary" in {
+    assertThrows[IllegalArgumentException] {
+      Resident(45, null.asInstanceOf[Gender])
+    }
   }
 
-  // specs on methods
-
-  "countOldManFloors" should "return the number of men older than the specified age" in {
-    1 shouldEqual 0 // Implement yourself
+  "countOldManFloors" should "return correct count of old men floors based on age" in {
+    countOldManFloors(buildingWithMultipleResidents, 35) shouldEqual 1
   }
 
-  it should "return 0 if there are no men older than the specified age" in {
-    1 shouldEqual 0 // Implement yourself
+  it should "return 0 when no men are older than the specified age" in {
+    countOldManFloors(buildingWithMultipleResidents, 45) shouldEqual 0
   }
 
-  it should "return 0 if there are no men at all in the building" in {
-    1 shouldEqual 0 // Implement yourself
+  it should "return 0 when there are no men in the building" in {
+    countOldManFloors(simpleAtticBuilding, 35) shouldEqual 0
   }
 
-  "womanMaxAge" should "find age of the oldest woman in the building" in {
-    1 shouldEqual 0 // Implement yourself
+  "womanMaxAge" should "return the age of the oldest woman in the building" in {
+    womanMaxAge(buildingWithMultipleResidents) shouldEqual Some(35)
   }
 
-  it should "return None if there are no women in the building" in {
-    1 shouldEqual 0 // Implement yourself
+  it should "return None when there are no women in the building" in {
+    womanMaxAge(simpleAtticBuilding) shouldEqual None
   }
 
-  "countCommercial" should "return number of commercial establishments in the building" in {
-    1 shouldEqual 0 // Implement yourself
+  "countCommercial" should "return the number of commercial establishments" in {
+    countCommercial(buildingWithShops) shouldEqual 1
   }
 
-  it should "return 0 if there are no commercial establishments in the building" in {
-    1 shouldEqual 0 // Implement yourself
+  it should "return 0 when there are no commercial establishments" in {
+    countCommercial(simpleAtticBuilding) shouldEqual 0
   }
 
-  "countCommercialAvg" should "return average commercial establishments through array of buildings" in {
-    1 shouldEqual 0 // Implement yourself
+  "countCommercialAvg" should "return the average number of commercial establishments across multiple buildings" in {
+    countCommercialAvg(List(buildingWithShops, simpleAtticBuilding)) shouldEqual 0.5
   }
 
-  it should "return 0 if there are no commercial establishments in the buildings" in {
-    1 shouldEqual 0 // Implement yourself
+  it should "return 0 when there are no commercial establishments in the buildings" in {
+    countCommercialAvg(List(simpleAtticBuilding)) shouldEqual 0.0
   }
 
-  it should "return 0 if there are no buildings" in {
-    1 shouldEqual 0 // Implement yourself
+  it should "return 0 when no buildings are provided" in {
+    countCommercialAvg(List.empty[Building]) shouldEqual 0.0
   }
 
-  "evenFloorsMenAvg" should "return average count of mens on even floors in the building" in {
-    1 shouldEqual 0 // Implement yourself
+  "evenFloorsMenAvg" should "return the average count of men on even floors" in {
+    val buildingWithMultipleFloors = Building("Even Towers", ResidentialFloor(maleResident, femaleResident, commercialFloorA))
+    evenFloorsMenAvg(buildingWithMultipleFloors) shouldEqual 1.0
   }
 
-  it should "return 0 if there are no mens in the building" in {
-    1 shouldEqual 0 // Implement yourself
+  it should "return 0 when there are no men in the building" in {
+    evenFloorsMenAvg(simpleAtticBuilding) shouldEqual 0.0
   }
 }
